361d9eee95519d834dbd636ea3ad1fb4189ecc07
kaur0762 <kaur0762@algonquinlive.com>
2023-03-14 18:33:13 -0400

    Add fully responsive layout

--- components/common/Layout/index.js
+++ components/common/Layout/index.js
@@ -5,25 +5,25 @@
  * Ghazaldeep Kaur <kaur0762@algonquinlive.com>
  */
 
-import React, { useState } from 'react'
+import React, { useState} from 'react'
 import Image from 'next/image';
 import Header from '../Header'
 import NavbarMenu from '../NavBar'
 import {IconMenu2 } from '@tabler/icons-react';
 
 const Layout = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  console.log(menuOpen);
+  const [menuOpen, setMenuOpen] = useState(true);
+
   function showMenu() {
     setMenuOpen(!menuOpen)
   }
+
   return (
     <>
     {menuOpen?
-    <div>
-      <NavbarMenu showMenu={menuOpen} showMenu={showMenu}/>
-      <div className="w-100">
-        <div className="w-full h-20 flex flex-row items-center bg-brand-blue-900 top-0 right-0 pl-2 pr-12">
+    <div className="h-screen relative z-0 flex bg-gray-500 sm:bg-transparent">
+      <div className="w-full">
+        <div className="w-full h-20 flex flex-row items-center bg-brand-blue-900 top-0 right-0 pl-2 pr-12 sm:hidden">
           <div className="self-start">
             <button className="visible center mt-4 mr-2" onClick={showMenu}>
               <IconMenu2 color="white" size={'3em'}/>
@@ -40,9 +40,13 @@ const Layout = () => {
         </div>
         <Header/>
       </div>
+      <div className="absolute inset-y-0 left-0 z-10">
+        <NavbarMenu showMenu={menuOpen} showMenu={showMenu}/>
+      </div>
     </div>:
     <div>
-      <div className="w-full h-20 flex flex-row items-center bg-brand-blue-900 top-0 right-0 pl-2 pr-12">
+    <div>
+      <div className="w-full h-20 flex flex-row items-center bg-brand-blue-900 top-0 right-0 pl-2 pr-12 sm:hidden">
         <div className="self-start">
           <button className="visible center mt-4 mr-2" onClick={showMenu}>
             <IconMenu2 color="white" size={'3em'}/>
@@ -58,6 +62,10 @@ const Layout = () => {
               />
       </div>
       <Header/>
+    </div>
+      <div className="absolute inset-y-0 left-0 z-10 hidden sm:block">
+        <NavbarMenu showMenu={menuOpen} showMenu={showMenu}/>
+      </div>
     </div>
     }
     </>
