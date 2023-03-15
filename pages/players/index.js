4be2b7fd4c7de23f8f5f62de2d08b2f14526d5e1
Verity Stevens <43241358+stev0298@users.noreply.github.com>
2023-03-15 00:28:50 -0400

    Hard-code test data in an array
    Create an array of "players" for test purposes.

--- pages/players/index.js
+++ pages/players/index.js
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavbarMenu from '@/components/NavBar';
 import SearchInput from '@/components/players/SearchInput';
 
 function Players() {
+	const playersList = [
+		{
+			id: 1,
+			firstName: 'Patrick',
+			lastName: 'King',
+			age: 33,
+			gender: 'Male',
+			location: 'North Gatineau',
+			teams: [
+				{
+					id: 'a',
+					name: 'The Juggernauts',
+					sport: 'Pick-up Sport',
+					role: 'Captain',
+				},
+			],
+		},
+		{
+			id: 2,
+			firstName: 'Jessie',
+			lastName: 'Summers',
+			age: 30,
+			gender: 'Female',
+			location: 'Westboro',
+			teams: [
+				{
+					id: 'b',
+					name: 'Lady Spikers',
+					sport: 'Volleyball',
+					role: 'Player',
+				},
+			],
+		},
+		{
+			id: 3,
+			firstName: 'Laura',
+			lastName: 'Banks',
+			age: 28,
+			gender: 'Female',
+			location: 'Kanata',
+			teams: [
+				{
+					id: '123',
+					name: 'Goody2Shoes',
+					sport: 'Soccer',
+					role: 'Player',
+				},
+				{
+					id: '456',
+					name: 'Lady Spikers',
+					sport: 'Volleyball',
+					role: 'Captain',
+				},
+				{
+					id: '789',
+					name: 'The Benchwarmers',
+					sport: 'Soccer',
+					role: 'Player',
+				},
+			],
+		},
+	];
+
+	const [players, setPlayers] = useState(playersList);
+
 	function handleSearch(ev) {
 		ev.preventDefault();
 		let search = document.getElementById('player-search').value;
