a28a1e41fc60607c9304f72a073aa50f94ff442f
tran0460 <tran0460@algonquinlive.com>
2023-03-14 20:53:53 -0400

    create get all teams function

--- utils/graphql.services.js
+++ utils/graphql.services.js
@@ -9,3 +9,18 @@ import { API, Auth } from 'aws-amplify';
 import * as mutations from '../graphql/mutations';
 import * as queries from '../graphql/queries';
 import { DivisionsEnum } from '@/models';
+
+/**
+ * Returns all teams in the database as an array
+ * @returns {[Object]} Team objects in an array
+ */
+export const getAllTeams = async () => {
+	try {
+		const resp = await API.graphql({
+			query: queries.listTeams,
+		});
+		return resp.data.listTeams.items;
+	} catch (e) {
+		console.warn(err);
+	}
+};
