d0468576ef4ae796c86352bf04bd97a4e5be3066
tran0460 <tran0460@algonquinlive.com>
2023-03-14 22:06:47 -0400

    add functions for get all players and all users

--- utils/graphql.services.js
+++ utils/graphql.services.js
@@ -1,17 +1,44 @@
 /**
- * Last updated: 2023-03-13
+ * Last updated: 2023-03-14
  *
  * Author(s):
  * Son Tran <tran0460@algonquinlive.com>
  */
 
-import { API, Auth } from 'aws-amplify';
-import * as mutations from '../graphql/mutations';
+import { API } from 'aws-amplify';
 import * as queries from '../graphql/queries';
-import { DivisionsEnum } from '@/models';
 
 /**
- * Returns all teams in the database as an array
+ * Returns all players in the database
+ * @returns {[Object]} Player objects in an array
+ */
+export const getAllPlayers = async () => {
+	try {
+		const resp = await API.graphql({
+			query: queries.listPlayersSoccers,
+		});
+		return resp.data.listPlayersSoccers.items;
+	} catch (e) {
+		console.warn(err);
+	}
+};
+/**
+ * Returns all users in the database
+ * @returns {[Object]} User objects in an array
+ */
+export const getAllUsers = async () => {
+	try {
+		const resp = await API.graphql({
+			query: queries.listUsers,
+		});
+		console.log(resp.data.listUsers.items);
+		return resp.data.listUsers.items;
+	} catch (e) {
+		console.warn(err);
+	}
+};
+/**
+ * Returns all teams in the database
  * @returns {[Object]} Team objects in an array
  */
 export const getAllTeams = async () => {
