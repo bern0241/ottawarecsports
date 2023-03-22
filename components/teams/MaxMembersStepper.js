3fd10bc505ee1e7acc1b0ff6b76f5f0b02160aac
tran0460 <tran0460@algonquinlive.com>
2023-03-21 22:23:53 -0400

    add min and max for input

--- components/teams/MaxMembersStepper.js
+++ components/teams/MaxMembersStepper.js
@@ -44,6 +44,8 @@ const MaxMembersStepper = ({ state = 0, setState }) => {
 				onChange={(e) => setState(e.target.value)}
 				type="number"
 				class="appearance-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				min={0}
+				max={20}
 			/>
 			<div className="absolute flex flex-row p-2 gap-2 right-0 top-1/2 -translate-y-1/2">
 				<MinusSvg />
