7052874f8294755b42bd33df29e733c320e2cf14
tran0460 <tran0460@algonquinlive.com>
2023-03-22 15:19:32 -0400

    display data

--- components/teams/TeamRow.js
+++ components/teams/TeamRow.js
@@ -10,6 +10,7 @@ import { IconEdit } from '@tabler/icons-react';
 import { IconTrash } from '@tabler/icons-react';
 
 export default function TeamRow({ team, setCurrentTeam }) {
+	const currentSeason = team.team_history[0];
 	return (
 		<tr key={team.id} className="border-b border-brand-neutral-300">
 			{/* odd:bg-white even:bg-brand-neutral-100 */}
@@ -22,11 +23,12 @@ export default function TeamRow({ team, setCurrentTeam }) {
 					{team.name}
 				</div>
 			</td>
-			{/* <td className="p-5">
-				{team.captain.firstName.charAt(0)}. {team.captain.lastName}
-			</td> */}
+			<td className="p-5">
+				{currentSeason.captains[0].charAt(0) || 'J'}.{' '}
+				{currentSeason.captains[0].split(' ')[1] || 'Doe'}
+			</td>
 			<td className="p-5">{team.sports || 'Soccer'}</td>
-			<td className="p-5">{team.teamMembers}/15</td>
+			<td className="p-5">{currentSeason.roster.length || 0}/15</td>
 			<td className="p-5">{team.notes}</td>
 			<td className="p-5">
 				<div className="flex">
