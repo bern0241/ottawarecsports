979861aaafe593ec67cf4b2389bb4a685ae8dfa1
Verity Stevens <43241358+stev0298@users.noreply.github.com>
2023-03-23 12:01:54 -0400

    Conditionally render Settings link
    Display Settings link if a user if signed in, else do not show it in the navigation bar.

--- components/common/NavBar/Links.js
+++ components/common/NavBar/Links.js
@@ -41,15 +41,19 @@ const NavbarSetup = () => {
 					<p className="pl-2">Admin Portal</p>
 				</Link>
 			)}
-			<Link
-				href="./settings"
-				className="flex flex-row p-2 text-white/70 hover:text-white focus:text-white"
-			>
-				<div className="pt-1 ">
-					<IconSettings size={'1.2em'} />
-				</div>
-				<p className="pl-2">Settings</p>
-			</Link>
+			{user ? (
+				<Link
+					href="./settings"
+					className="flex flex-row p-2 text-white/70 hover:text-white focus:text-white"
+				>
+					<div className="pt-1 ">
+						<IconSettings size={'1.2em'} />
+					</div>
+					<p className="pl-2">Settings</p>
+				</Link>
+			) : (
+				''
+			)}
 		</div>
 	);
 };
