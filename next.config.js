9eb4aee0e1bdd4626f93372e24839e643e96a43b
[Justin Bernard] <[bern0241@algonquinlive.com]>
2023-03-23 03:54:17 -0400

    Add code inside next.config.js for accessing environment variables in host deployments

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+
+	env: {
+		NEXT_PUBLIC_ACCESS_KEY_ID: process.env.NEXT_PUBLIC_ACCESS_KEY_ID,
+		NEXT_PUBLIC_SECRET_ACCESS_KEY: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY,
+	},
 };
 
 module.exports = nextConfig;
