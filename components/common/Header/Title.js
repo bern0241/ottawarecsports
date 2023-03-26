330426893da9efa74f861b3de3bcc14ac07fcf63
[Justin Bernard] <[bern0241@algonquinlive.com]>
2023-03-26 01:23:57 -0400

    Add links for all pages

--- components/common/Header/Title.js
+++ components/common/Header/Title.js
@@ -6,12 +6,18 @@
  */
 
 import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const HeaderTitle = () => {
 	return (
 		<div className="p-1 pt-0 pl-7">
 			<div className='flex flex-col gap-3'>
-				<p className="font-light text-[.8rem]">Home / Settings</p>
+				<div className='flex flex-row'>
+					<Link href="/" className="font-light text-[.8rem]">Home</Link>
+						<p className="font-light text-[.8rem]">&nbsp;/&nbsp;</p>
+					<Link href="/settings" className="font-light text-[.8rem]">Settings</Link>
+				</div>
 				<p className="font-semibold text-[1.8rem]">Settings</p>
 			</div>
 		</div>
