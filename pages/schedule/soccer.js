adf8ca669cf4515f370e4d34716fbba80bd733a6
tran0460 <tran0460@algonquinlive.com>
2023-03-29 14:47:17 -0400

    add matches tables for styling

--- pages/schedule/soccer.js
+++ pages/schedule/soccer.js
@@ -3,11 +3,13 @@ import { Button } from 'flowbite-react';
 import { IconRepeat } from '@tabler/icons-react';
 import DivisionRow from '@/components/schedule/DivisionRow';
 import ChangeSeasonModal from '@/components/schedule/ChangeSeasonModal';
+import MatchesTable from '@/components/schedule/MatchesTable';
 
 const soccer = () => {
 	const [currentLeague, setCurrentLeague] = useState('');
 	const [currentSeason, setCurrentSeason] = useState('');
 	const [modalVisible, setModalVisible] = useState(false);
+	const [selectedDivision, setSelectedDivision] = useState();
 	return (
 		<>
 			<main className="w-full flex flex-col gap-6 p-8">
@@ -20,6 +22,7 @@ const soccer = () => {
 						setCurrentLeague={setCurrentLeague}
 					/>
 				)}
+				<MatchesTable />
 				{/* Results */}
 				<div className="flex flex-col w-full h-auto bg-white border border-brand-neutral-300 rounded-md">
 					<div className="flex justify-between py-3 px-5 border-b border-brand-neutral-300 items-center">
