308e430a18546202fb847483a975caac9572d3b9
tran0460 <tran0460@algonquinlive.com>
2023-03-29 12:13:58 -0400

    create team division row component with placeholder data

--- components/schedule/DivisionRow.js
+++ components/schedule/DivisionRow.js
@@ -1,43 +1,32 @@
 import React from 'react';
 
 const DivisionRow = () => {
+	const CalendarIcon = () => (
+		<svg width={18} height={18} fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M1.34 17.143c-.357 0-.67-.129-.938-.386C.134 16.5 0 16.2 0 15.857V2.571c0-.342.134-.642.402-.9.268-.257.58-.385.938-.385h1.452V0h1.451v1.286h7.593V0h1.452v1.286h1.451c.358 0 .67.128.938.385.268.258.402.558.402.9v6.45h-1.34V6.643H1.34v9.214h7.125v1.286H1.34Zm15.834-4.693-1.586-1.521.648-.622a.65.65 0 0 1 .469-.171.65.65 0 0 1 .469.171l.647.622c.12.114.179.264.179.45 0 .185-.06.335-.179.45l-.647.621ZM9.804 18v-1.521l4.824-4.629 1.585 1.521L11.39 18H9.804ZM1.34 5.357h13.4V2.571H1.34v2.786Z"
+				fill="#023059"
+				fillOpacity={0.8}
+			/>
+		</svg>
+	);
 	return (
 		<tr
-			key={team.id}
+			// key={team.id}
 			className="border-b border-brand-neutral-300"
-			onClick={navigateToProfile}
+			// onClick={navigateToProfile}
 		>
 			{/* odd:bg-white even:bg-brand-neutral-100 */}
 			<td className="p-5 font-medium">
 				<div className="flex items-center">
-					<img
-						src={profileImage}
-						className="rounded-full mr-5 w-[60px] h-[60px] object-cover"
-					></img>
-					{team.name}
+					<p>{'Super Team'}</p>
 				</div>
 			</td>
-			<td className="p-5">
-				{currentSeason
-					? currentSeason.captains.map((captain, index) => (
-							<span key={index}>{userName}</span>
-					  ))
-					: 'John Doe'}
+			<td className="p-5">{'A'}</td>
+			<td className="p-5">Co-ed</td>
+			<td className="p-5 flex justify-center">
+				<CalendarIcon />
 			</td>
-			<td className="p-5">{team.sports || 'Soccer'}</td>
-			<td className="p-5">
-				{currentSeason ? currentSeason.roster.length : 0}/15
-			</td>
-			<td className="p-5">{team.notes ? team.notes : 'N/A'}</td>
-			{/* <td className="p-5">
-        <div className="flex">
-            <IconEdit
-                className="text-brand-blue-900 mr-3"
-                onClick={() => setCurrentTeam(team)}
-            />
-            <IconTrash className="text-brand-orange-800 hover:bg-blue-400" />
-        </div>
-    </td> */}
 		</tr>
 	);
 };
