b990bce31613753410ee14b6281ddf0452e60458
tran0460 <tran0460@algonquinlive.com>
2023-03-29 13:02:29 -0400

    create change league modal

--- pages/schedule/soccer.js
+++ pages/schedule/soccer.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
 import { Button } from 'flowbite-react';
 import { IconRepeat } from '@tabler/icons-react';
 import DivisionRow from '@/components/schedule/DivisionRow';
+import ChangeSeasonModal from '@/components/schedule/ChangeSeasonModal';
 
 const soccer = () => {
+	const [currentLeague, setCurrentLeague] = useState('');
+	const [currentSeason, setCurrentSeason] = useState('');
+	const [modalVisible, setModalVisible] = useState(false);
 	return (
 		<>
 			<main className="w-full flex flex-col gap-6 p-8">
+				{modalVisible && (
+					<ChangeSeasonModal
+						setModalVisible={setModalVisible}
+						currentSeason={currentSeason}
+						setCurrentSeason={setCurrentSeason}
+						currentLeague={currentLeague}
+						setCurrentLeague={setCurrentLeague}
+					/>
+				)}
 				{/* Results */}
 				<div className="flex flex-col w-full h-auto bg-white border border-brand-neutral-300 rounded-md">
 					<div className="flex justify-between py-3 px-5 border-b border-brand-neutral-300 items-center">
@@ -14,7 +27,7 @@ const soccer = () => {
 						<Button
 							pill={true}
 							className="px-[1.75rem] bg-blue-900 hover:bg-blue-800 px-4"
-							// onClick={() => setModalVisible(true)}
+							onClick={() => setModalVisible(true)}
 						>
 							<IconRepeat className="mr-2 h-5 w-5" />
 							Change
@@ -36,14 +49,6 @@ const soccer = () => {
 							<DivisionRow />
 							<DivisionRow />
 							<DivisionRow />
-							<tr>
-								<td
-									colSpan={6}
-									className="pt-8 pb-4 text-center text-sm text-brand-neutral-800"
-								>
-									End
-								</td>
-							</tr>
 						</tbody>
 					</table>
 				</div>
