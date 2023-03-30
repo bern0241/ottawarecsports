8b982379848381adb5143bcd862fd76baf209f36
kaur0762 <kaur0762@algonquinlive.com>
2023-03-29 20:47:28 -0400

    Added profile pic to player profile page

--- pages/players/[id].js
+++ pages/players/[id].js
@@ -12,13 +12,14 @@ import { Button } from 'flowbite-react';
 import { IconChevronLeft } from '@tabler/icons-react';
 import AWS from 'aws-sdk';
 import Image from 'next/image';
-import { getPlayersByUsername } from '@/utils/graphql.services';
+import { getImageFromS3, getPlayersByUsername } from '@/utils/graphql.services';
 
 export default function PlayerProfile() {
 	const router = useRouter();
 	const userId = router.query.id;
 	const [player, setPlayer] = useState(); // Player Table
 	const [user, setUser] = useState(); // Cognito User
+	const [profileImage, setProfileImage] = useState('');
 	var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
 	useEffect(() => {
@@ -29,8 +30,16 @@ export default function PlayerProfile() {
 		fetchPlayerCognito();
 	}, [userId])
 
+	useEffect(() => {
+		if(!user) {
+			return
+		}
+		getPicture();
+	}, [user])
+
 	const fetchPlayer = async () => {
 		const data = await getPlayersByUsername(userId);
+		console.log('Player', data);
 		setPlayer(data[0]);
 	}
 
@@ -46,6 +55,15 @@ export default function PlayerProfile() {
 					setUser(data);
 		});
 	}
+
+	const getPicture = async () => {
+		if (user.UserAttributes.find(o => o.Name === 'picture')['Value'] === 'none') {
+			setProfileImage(null);
+		} else {
+		const url = await getImageFromS3(user.UserAttributes.find(o => o.Name === 'picture')['Value']);
+		setProfileImage(url);
+		}
+	};
 
 	return (
 		<>
@@ -67,12 +85,10 @@ export default function PlayerProfile() {
 
 					<div className="grid grid-cols-3 gap-4 p-8">
 						{/* Player Avatar */}
-						<div className="col-span-3 sm:col-span-1 row-span-2 flex flex-col gap-4">
+						<div className="col-span-3 md:col-span-1 row-span-2 flex flex-col gap-4">
 							<img
-								src={'https://api.lorem.space/image/face?w=200&h=200'}
-								className="rounded-full self-center"
-								width="200"
-								height="200"
+								src={`${profileImage ? profileImage : "/images/defaultProfilePic.jpeg"}`}
+								className="rounded-full self-center w-[200px] h-[200px] object-cover"
 								alt="Player profile image."
 							></img>
 							<div className="flex justify-center gap-1">
@@ -83,7 +99,7 @@ export default function PlayerProfile() {
 						</div>
 
 						{/* Player Information */}
-						<div className="col-span-3 sm:col-span-2 grid grid-cols-2 gap-y-4 gap-x-8">
+						<div className="col-span-3 md:col-span-2 grid grid-cols-2 gap-y-4 gap-x-8">
 							<div className="col-span-1 flex flex-col">
 								<h3 className="mb-1 font-light">First Name</h3>
 								<div className="py-2 px-3 border rounded-md border-brand-blue-900/25 font-medium">
@@ -114,7 +130,7 @@ export default function PlayerProfile() {
 						</div>
 
 						{/* Player Teams */}
-						<div className="col-span-3 sm:col-span-2 border rounded-md border-brand-blue-900/25">
+						<div className="col-span-3 md:col-span-2 border rounded-md border-brand-blue-900/25">
 							<table className="border-collapse table-fixed w-full overflow-hidden rounded-md">
 								<thead className="bg-brand-neutral-100 border-b border-brand-blue-900/25">
 									<tr className="text-left">
@@ -130,17 +146,25 @@ export default function PlayerProfile() {
 									</tr>
 								</thead>
 								<tbody>
+									{player ? 
 									<tr className="font-light">
 										<td className="py-2 px-3">Soccer</td>
-										<td className="py-2 px-3">Team Name</td>
-										<td className="py-2 px-3">Player</td>
-									</tr>
+										<td className="py-2 px-3">
+											{player && player.soccer_stats.teams }
+										</td>
+										<td className="py-2 px-3">
+											{player && player.soccer_stats.role }
+										</td>
+									</tr>:
+									<tr className="text-center text-brand-neutral-800">
+										No Data to Show
+									</tr>}
 								</tbody>
 							</table>
 						</div>
 
 						{/* Player Game History */}
-						<div className="col-span-3">
+						{/* <div className="col-span-3">
 							<h2 className="mb-1 font-light">Games History</h2>
 
 							<div className="col-span-3 border rounded-md border-brand-blue-900/25">
@@ -167,7 +191,7 @@ export default function PlayerProfile() {
 									</tbody>
 								</table>
 							</div>
-						</div>
+						</div> */}
 					</div>
 				</div>
 			</main>
