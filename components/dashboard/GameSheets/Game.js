e1efc873f5790f3cf935d8b778c0156e725abf0c
Verity Stevens <43241358+stev0298@users.noreply.github.com>
2023-04-03 13:28:51 -0400

    Create game sheets component for dashboard

--- components/dashboard/GameSheets/Game.js
+++ components/dashboard/GameSheets/Game.js
@@ -1,21 +1,43 @@
 import React from 'react';
-import { IconClock, IconCalendarEvent, IconMapPin } from '@tabler/icons-react';
+import {
+	IconClock,
+	IconCalendarEvent,
+	IconMapPin,
+} from '@tabler/icons-react';
 import TeamNameAndImage from '@/components/schedule/TeamNameAndImage';
 
 export default function Game() {
 	return (
 		<>
-			<div className='w-full bg-blue-500 p-8 grid grid-cols-4'>
-                <div className='col-span-1'>Hi</div>
-                <div className='col-span-2'>Hi</div>
-                <div className='col-span-1 flex flex-col gap-3'>
-                    <div className='flex justify-between gap-3 text-sm'>
-                        <span className='flex items-center gap-1'><IconCalendarEvent /> April 2nd, 2023</span>
-                        <span className='flex items-center gap-1'><IconClock /> 02:30 PM</span>
-                    </div>
-                    <div className='flex gap-1 text-sm'><IconMapPin /> Algonquin Dome Field #1</div>
-                </div>
-            </div>
+			<div className="w-full p-8 flex flex-row justify-between items-center gap-4">
+				<div className="min-w-[0px] flex justify-center">
+                <img src="/images/soccerball_orange.png" />
+				</div>
+				<div>
+					<div className="font-medium flex flex-row gap-8 items-center">
+						<TeamNameAndImage />
+						<span className="border-2 border-brand-orange-800 rounded-xl px-[47px] py-[10px] flex flex-row items-center h-fit gap-1">
+							<p>{0}</p>
+							<p>:</p>
+							<p>{0}</p>
+						</span>
+						<TeamNameAndImage reverse={true} />
+					</div>
+				</div>
+				<div className="flex flex-col gap-3 mx-8">
+					<div className="flex justify-between gap-3 text-sm">
+						<span className="flex items-center gap-1">
+							<IconCalendarEvent /> April 2nd, 2023
+						</span>
+						<span className="flex items-center gap-1">
+							<IconClock /> 02:30 PM
+						</span>
+					</div>
+					<div className="flex gap-1 text-sm">
+						<IconMapPin /> Algonquin Dome Field #1
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
--- components/dashboard/GameSheets/index.js
+++ components/dashboard/GameSheets/index.js
@@ -5,20 +5,30 @@
  * Verity Stevens <stev0298@algonquinlive.com>
  */
 
-import React from 'react'
-import Game from './Game'
+import React from 'react';
+import Game from './Game';
+import { IconAlertCircleFilled } from '@tabler/icons-react';
 
 export default function GameSheets() {
-  return (
-    <section id="game-sheets" className="col-span-4">
-        <div className="bg-white border border-brand-neutral-300 rounded-md">
-				<div className="flex items-center justify-start border-b border-brand-neutral-300 px-3 py-2">
+	return (
+		<section id="game-sheets" className="col-span-4">
+			<div className="bg-white border border-brand-neutral-300 rounded-md">
+				<div className="flex items-center justify-between border-b border-brand-neutral-300 px-3 py-2">
 					<h1 className="text-md font-medium">Update Game Scores</h1>
+					<div className="flex items-center bg-brand-orange-200 rounded-full">
+						<IconAlertCircleFilled
+							size={24}
+							className="text-brand-orange-800"
+						/>
+						<span className="text-xs font-medium text-brand-orange-900 mx-2">
+							These games need your attention!
+						</span>
+					</div>
 				</div>
 				<div className="">
-                    <Game />
+					<Game />
 				</div>
 			</div>
-    </section>
-  )
+		</section>
+	);
 }
