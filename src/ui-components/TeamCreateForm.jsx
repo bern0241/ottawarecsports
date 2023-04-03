6731d1da85a8d60651547579674d115c38d0b849
Verity Stevens <43241358+stev0298@users.noreply.github.com>
2023-04-03 13:32:14 -0400

    Run amplify pull

--- src/ui-components/TeamCreateForm.jsx
+++ src/ui-components/TeamCreateForm.jsx
@@ -34,9 +34,16 @@ function ArrayField({
   defaultFieldValue,
   lengthLimit,
   getBadgeText,
+  errorMessage,
 }) {
   const labelElement = <Text>{label}</Text>;
-  const { tokens } = useTheme();
+  const {
+    tokens: {
+      components: {
+        fieldmessages: { error: errorStyles },
+      },
+    },
+  } = useTheme();
   const [selectedBadgeIndex, setSelectedBadgeIndex] = React.useState();
   const [isEditing, setIsEditing] = React.useState();
   React.useEffect(() => {
@@ -139,6 +146,11 @@ function ArrayField({
           >
             Add item
           </Button>
+          {errorMessage && hasError && (
+            <Text color={errorStyles.color} fontSize={errorStyles.fontSize}>
+              {errorMessage}
+            </Text>
+          )}
         </>
       ) : (
         <Flex justifyContent="flex-end">
@@ -157,7 +169,6 @@ function ArrayField({
           <Button
             size="small"
             variation="link"
-            color={tokens.colors.brand.primary[80]}
             isDisabled={hasError}
             onClick={addItem}
           >
@@ -226,9 +237,10 @@ export default function TeamCreateForm(props) {
     currentValue,
     getDisplayValue
   ) => {
-    const value = getDisplayValue
-      ? getDisplayValue(currentValue)
-      : currentValue;
+    const value =
+      currentValue && getDisplayValue
+        ? getDisplayValue(currentValue)
+        : currentValue;
     let validationResponse = validateField(value, validations[fieldName]);
     const customValidator = fetchByPath(onValidate, fieldName);
     if (customValidator) {
@@ -246,7 +258,7 @@ export default function TeamCreateForm(props) {
       minute: "2-digit",
       calendar: "iso8601",
       numberingSystem: "latn",
-      hour12: false,
+      hourCycle: "h23",
     });
     const parts = df.formatToParts(date).reduce((acc, part) => {
       acc[part.type] = part.value;
@@ -482,7 +494,8 @@ export default function TeamCreateForm(props) {
         currentFieldValue={currentCaptainsValue}
         label={"Captains"}
         items={captains}
-        hasError={errors.captains?.hasError}
+        hasError={errors?.captains?.hasError}
+        errorMessage={errors?.captains?.errorMessage}
         setFieldValue={setCurrentCaptainsValue}
         inputFieldRef={captainsRef}
         defaultFieldValue={""}
