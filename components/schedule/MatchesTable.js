ecb8d9cce707520cca8644d114cd077ddd3d9473
tran0460 <tran0460@algonquinlive.com>
2023-04-04 19:56:13 -0400

    add function for getting all the dates from matches

--- components/schedule/MatchesTable.js
+++ components/schedule/MatchesTable.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import DropdownInput from '../common/DropdownInput';
 import MatchRow from './MatchRow';
 
 const MatchesTable = ({ title = 'Scheduled matches', matches }) => {
+	const [matchDates, setMatchDates] = useState([]);
+	const dropdownRef = useRef();
 	const CircleArrowDown = () => (
 		<svg width={16} height={17} fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
@@ -17,11 +20,34 @@ const MatchesTable = ({ title = 'Scheduled matches', matches }) => {
 		let timeB = matchB.date ? matchB.date : matchB.createdAt;
 		return Date.parse(timeA) - Date.parse(timeB);
 	});
+
+	// go through the sorted match list, get all the dates and return them as an array
+	const returnDateArray = () => {
+		let dateArray = [];
+		timeSortedMatches.map((match) => {
+			const matchDate = match.date
+				? new Date(Date.parse(match.date))
+				: new Date(Date.parse(match.createdAt));
+			const matchDateString = matchDate.toDateString();
+			// remove the year and add a comma after the day of week
+			const dateWithoutWeekday = matchDateString.substring(
+				4,
+				matchDateString.length
+			);
+			if (!dateArray.includes(dateWithoutWeekday))
+				dateArray.push(dateWithoutWeekday);
+		});
+		return dateArray;
+	};
+	useEffect(() => {
+		setMatchDates(returnDateArray());
+	}, [matches]);
 	return (
 		<>
 			<div className="flex flex-col w-full h-auto bg-white border border-brand-neutral-300 rounded-md">
 				<div className="flex justify-between py-[15px] px-[20px] border-b border-brand-neutral-300 items-center w-12/12">
 					<h1 className="text-base self-center font-medium">{title}</h1>
+					<DropdownInput hidden ref={dropdownRef} options={matchDates} />
 					<button
 						className="flex items-center justify-between py-[6.5px] px-3 gap-7 font-medium text-sm rounded-3xl border border-brand-blue-900"
 						// onClick={() => setModalVisible(true)}
