a033df49816408eab6b8f75abdaae0aa1254549f
[Justin Bernard] <[bern0241@algonquinlive.com]>
2023-04-04 23:44:39 -0400

    Change player roles from captain

--- components/admin-portal/ACPLeague/TeamTable/TeamCard.js
+++ components/admin-portal/ACPLeague/TeamTable/TeamCard.js
@@ -10,11 +10,12 @@ import React, { useState, useEffect } from 'react';
 import RemoveTeamModal from './RemoveTeamModal';
 import { IconTrash, IconEdit } from '@tabler/icons-react';
 import { useRouter } from 'next/router';
+import { getImageFromS3 } from '@/utils/graphql.services';
  
  export default function TeamCard({ teamDivision, listTeamDivisionsFunc }) {
-    //  const [editModal, setEditModal] = useState(false);
      const [removeModal, setRemoveModal] = useState(false);
      const [membersCount, setMembersCount] = useState(0);
+     const [teamImage, setTeamImage] = useState(null);
      const router = useRouter();
 
      useEffect(() => {
@@ -22,7 +23,17 @@ import { useRouter } from 'next/router';
       // if (team.Players.items) {
       //   setMembersCount(team.Players.items.length);
       // }
-     }, [])
+      getTeamImage();
+     }, [listTeamDivisionsFunc])
+
+     const getTeamImage = async () => {
+      if (teamDivision.team.team_picture === null || teamDivision.team.team_picture === '') {
+          setTeamImage(null);
+      } else {
+          const url = await getImageFromS3(teamDivision.team.team_picture);
+          setTeamImage(url);
+      }
+  }
  
      const removeTeamFunc = (e) => {
          e.stopPropagation();
@@ -37,11 +48,19 @@ import { useRouter } from 'next/router';
      return (
          <>
          <tr onClick={(e) => goToTeamPage(e)} class="bg-white border border-gray-400 cursor-pointer">
-                 <th scope="row" class="relative px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                 <th scope="row" class="relative px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white flex items-center gap-2">
+                  <img
+                        style={{ objectFit: 'cover' }}
+                        width={132}
+                        height={132}
+                        className="w-[3rem] h-[3rem] rounded-full shadow-md border border-black"
+                        src={`${teamImage ? teamImage : "/images/defaultProfilePic.jpeg"}`}
+                    />
                     {teamDivision.team.name}
                  </th>
                  <td class="px-6 py-3">
-                    Justin Bernard
+                 
+                   <p>Justin Bernard</p>
                  </td>
                  <td class="px-6 py-3">
                      {membersCount}
