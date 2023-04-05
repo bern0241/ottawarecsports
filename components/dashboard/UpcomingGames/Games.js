de1c3e88781f040862565267cfb7528e736b5d3e
Verity Stevens <43241358+stev0298@users.noreply.github.com>
2023-04-04 21:47:38 -0400

    Query all games for currently logged in user

--- components/dashboard/UpcomingGames/Games.js
+++ components/dashboard/UpcomingGames/Games.js
@@ -5,19 +5,47 @@
  * Verity Stevens <stev0298@algonquinlive.com>
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { API } from '@aws-amplify/api';
-import { getDivisionWithTeams } from '@/src/graphql/custom-queries';
+import { getGamesByTeam } from '@/src/graphql/custom-queries';
+import { getPlayersByUsername } from '@/utils/graphql.services';
+import { useUser } from '@/context/userContext';
 
 export default function Games() {
+	// Get currently logged in user:
+	const [user, setUser, authRoles, setAuthRoles] = useUser();
+	const [userId, setUserId] = useState();
+	const [playerData, setPlayerData] = useState();
+	const [teams, setTeams] = useState();
 
-	// const getGames = async () => {
-    //     const apiData = await API.graphql(
-    //         { query: getDivisionWithTeams, 
-    //         variables: { teamID: id }
-    //     });
-    //     console.log('See here', apiData.data);
-    // }
+	useEffect(() => {
+		if (!user) return;
+		setUserId(user.username);
+	}, [user]);
+
+	useEffect(() => {
+		if (!userId) return;
+		fetchPlayer();
+	}, [userId]);
+
+	useEffect(() => {
+		getGames(playerData.teamID);
+	}, [playerData]);
+
+	const fetchPlayer = async () => {
+		const data = await getPlayersByUsername(userId);
+		if (data) {
+			setPlayerData(data[0]);
+		}
+	};
+
+	const getGames = async (id) => {
+		const apiData = await API.graphql({
+			query: getGamesByTeam,
+			variables: { teamId: id },
+		});
+		console.log('Games', apiData.data.listGames.items.length);
+	};
 
 	const soccerGames = [
 		{
@@ -38,7 +66,10 @@ export default function Games() {
 		<div className="flex flex-row lg:flex-col gap-8 md:gap-4 xl:gap-1">
 			{soccerGames &&
 				soccerGames.map((game, index) => (
-					<div key={index} className="flex flex-col xl:flex-row justify-between xl:gap-3">
+					<div
+						key={index}
+						className="flex flex-col xl:flex-row justify-between xl:gap-3"
+					>
 						<div className="font-light text-sm">{game.day}</div>
 						<div className="font-medium">{game.matches.length} Matches</div>
 					</div>
