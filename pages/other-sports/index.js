9dd241c0595f62bd911c4da8fcf484943ccd317b
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2023-04-11 11:04:36 -0400

    Add other-sports page for volleyball and other sports

---
+++ pages/other-sports/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OtherSports = () => {
+	return (
+		<div className="flex flex-col justify-center items-center">
+			<h2 className="text-xl sm:text-2xl">Not Available.</h2>
+			<p>Please check back again on a later time.</p>
+		</div>
+	);
+};
+
+export default OtherSports;
