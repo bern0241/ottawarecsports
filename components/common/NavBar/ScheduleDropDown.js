41052805507421bddac521f9c8cce1206bc95039
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2023-04-11 11:15:00 -0400

    Update href of non soccer sports to other-sports on schedule drop down

--- components/common/NavBar/ScheduleDropDown.js
+++ components/common/NavBar/ScheduleDropDown.js
@@ -15,12 +15,11 @@ import {
 	IconChevronRight,
 } from '@tabler/icons-react';
 
-const ScheduleDropDown = ({openDropdown, toggle, dropdownMenuNames}) => {
-
+const ScheduleDropDown = ({ openDropdown, toggle, dropdownMenuNames }) => {
 	return (
 		<div className="text-white/70 hover:text-white focus:text-white">
 			<button
-				onClick={() => toggle("schedule")}
+				onClick={() => toggle('schedule')}
 				className="px-5 py-2 flex ml-2 flex-row justify-between w-full"
 			>
 				<div className="flex flex-row align-middle">
@@ -30,25 +29,48 @@ const ScheduleDropDown = ({openDropdown, toggle, dropdownMenuNames}) => {
 					<span className="font-medium text-md pl-2">Schedule</span>
 				</div>
 				<div className="pt-1 mr-2">
-					<IconChevronRight style={{transition: '320ms', transform: openDropdown === "schedule" ? 'rotate(90deg)' : 'rotate(0deg)'}} size={'1.2em'} />
+					<IconChevronRight
+						style={{
+							transition: '320ms',
+							transform:
+								openDropdown === 'schedule' ? 'rotate(90deg)' : 'rotate(0deg)',
+						}}
+						size={'1.2em'}
+					/>
 				</div>
 			</button>
 			<div
-				// style={{
-				// 	display: showMe ? 'block' : 'none',
-				// }}
+			// style={{
+			// 	display: showMe ? 'block' : 'none',
+			// }}
 			>
-				<div className={`border-x-[1px] border-black flex flex-col font-regular text-sm pl-12 bg-blue-100 text-black/70 transition-all duration-[320ms] overflow-hidden ${openDropdown === "schedule" ? 'h-[7.3rem]' : 'h-0'}`}>
-					<Link href="/schedule/soccer" className="my-1 hover:font-bold focus:font-bold">
+				<div
+					className={`border-x-[1px] border-black flex flex-col font-regular text-sm pl-12 bg-blue-100 text-black/70 transition-all duration-[320ms] overflow-hidden ${
+						openDropdown === 'schedule' ? 'h-[7.3rem]' : 'h-0'
+					}`}
+				>
+					<Link
+						href="/schedule/soccer"
+						className="my-1 hover:font-bold focus:font-bold"
+					>
 						Soccer
 					</Link>
-					<Link href="/schedule/volleyball" className="my-1 hover:font-bold focus:font-bold">
+					<Link
+						href="/other-sports"
+						className="my-1 hover:font-bold focus:font-bold"
+					>
 						Volleyball
 					</Link>
-					<Link href="/schedule/multi-sport" className="my-1 hover:font-bold focus:font-bold">
+					<Link
+						href="/other-sports"
+						className="my-1 hover:font-bold focus:font-bold"
+					>
 						Multi-Sport
 					</Link>
-					<Link href="/schedule/pickup-sport" className="my-1 hover:font-bold focus:font-bold">
+					<Link
+						href="/other-sports"
+						className="my-1 hover:font-bold focus:font-bold"
+					>
 						Pick-up Sport
 					</Link>
 				</div>
