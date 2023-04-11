99f8b963997a953b0ddaf452b7eb81db2f4b59c3
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2023-04-10 22:12:28 -0400

    Update responsiveness on sign in screen

--- components/common/OrsLogo.js
+++ components/common/OrsLogo.js
@@ -10,9 +10,9 @@ import Image from 'next/image';
 
 const OrsLogo = () => {
 	return (
-		<header className="flex flex-row justify-center items-center pr-6 pl-6 gap-0.5">
+		<header className="flex flex-row justify-center items-center gap-0.5 sm:px-px sm:gap-1">
 			<Image src="/Logo.svg" alt="ORS Logo" width={90} height={90} priority />
-			<h1 className="text-2xl md:text-3xl font-semibold text-center">
+			<h1 className="text-2xl sm:text-3xl font-semibold text-center">
 				Ottawa Rec Sports League App
 			</h1>
 		</header>
