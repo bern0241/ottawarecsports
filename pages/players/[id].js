bdd22db04643c020e9773f24c2a437301dbfd736
Verity Stevens <43241358+stev0298@users.noreply.github.com>
2023-04-11 19:20:27 -0400

    Fix mobile responsiveness of fields

--- pages/players/[id].js
+++ pages/players/[id].js
@@ -202,7 +202,7 @@ export default function PlayerProfile() {
 								</div>
 							</div>
 
-							<div className="col-span-1 flex flex-col">
+							<div className="col-span-2 md:col-span-1 flex flex-col">
 								<h3 className="mb-1 font-light">Location</h3>
 								<div className="py-2 px-3 border rounded-md border-brand-blue-900/25 font-medium">
 									{user &&
@@ -212,7 +212,7 @@ export default function PlayerProfile() {
 								</div>
 							</div>
 
-							<div className="col-span-1 flex flex-col">
+							<div className="col-span-2 md:col-span-1 flex flex-col">
 								<h3 className="mb-1 font-light">Gender</h3>
 								<div className="py-2 px-3 border rounded-md border-brand-blue-900/25 font-medium">
 									{user &&
