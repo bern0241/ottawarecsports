04daa6dc0947a0e73231004e1a6620e45ae7b3fb
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2023-04-11 10:07:28 -0400

    Update ORS logo width on small screen

--- components/common/OrsLogo.js
+++ components/common/OrsLogo.js
@@ -11,7 +11,14 @@ import Image from 'next/image';
 const OrsLogo = () => {
 	return (
 		<header className="flex flex-row justify-center items-center gap-0.5 sm:px-px sm:gap-1">
-			<Image src="/Logo.svg" alt="ORS Logo" width={90} height={90} priority />
+			<Image
+				src="/Logo.svg"
+				alt="ORS Logo"
+				width={90}
+				height={90}
+				priority
+				className="sm:w-max"
+			/>
 			<h1 className="text-2xl sm:text-3xl font-semibold text-center">
 				Ottawa Rec Sports League App
 			</h1>
