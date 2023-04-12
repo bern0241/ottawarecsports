8fdea83f7a0faf16a1f0f786702aecf0d93b4f9b
Leo <bata0020@algonquinlive.com>
2023-04-12 16:36:37 -0400

    Update sign up view height to h-screen

--- components/signup/SignUpView.js
+++ components/signup/SignUpView.js
@@ -128,123 +128,131 @@ export default function SignUpView({ setUiState, email, setEmail }) {
 
 	return (
 		<div className="h-fit bg-white">
-      <div className="flex flex-col lg:flex-row justify-between align-middle bg-white h-full lg:h-screen">
-			<div>
-				<div className="w-80 h-screen bg-brand-blue-900 top-0 left-0 hidden lg:block"></div>
-				<div className="w-full h-20 bg-brand-blue-900 top-0 right-0 lg:hidden mb-8"></div>
-			</div>
-			<div className="flex justify-center items-center w-full h-full mb-5">
-				<div className="flex flex-col gap-3 w-80 sm:w-96 sm:gap-4">
-					<OrsLogo />
-					<div className="flex flex-row items-center">
-						<h2 className="text-xl font-semibold my-2 sm:text-2xl grow">
-							Sign Up
-						</h2>
-						<p className="text-sm">* Required</p>
-					</div>
-					<div className="flex flex-col justify-center items-center">
-						<form className="flex flex-col gap-2 w-80 sm:w-96">
-							<div className="flex flex-col gap-2 sm:flex-row sm:gap-3">
+			<div className="flex flex-col lg:flex-row justify-between align-middle bg-white h-full lg:h-screen">
+				<div>
+					<div className="w-80 h-screen bg-brand-blue-900 top-0 left-0 hidden lg:block"></div>
+					<div className="w-full h-20 bg-brand-blue-900 top-0 right-0 lg:hidden mb-8"></div>
+				</div>
+				<div className="flex justify-center items-center w-full h-screen mb-5">
+					<div className="flex flex-col gap-3 w-80 sm:w-96 sm:gap-4">
+						<OrsLogo />
+						<div className="flex flex-row items-center">
+							<h2 className="text-xl font-semibold my-2 sm:text-2xl grow">
+								Sign Up
+							</h2>
+							<p className="text-sm">* Required</p>
+						</div>
+						<div className="flex flex-col justify-center items-center">
+							<form className="flex flex-col gap-2 w-80 sm:w-96">
+								<div className="flex flex-col gap-2 sm:flex-row sm:gap-3">
+									<Label
+										htmlFor="firstname"
+										value="First Name"
+										className="sr-only"
+									/>
+									<TextInput
+										id="firstname"
+										type="firstname"
+										placeholder="First Name *"
+										onChange={(e) => setFirstName(e.target.value)}
+										required={true}
+										className="w-full"
+									/>
+									<Label
+										htmlFor="lastname"
+										value="Last Name"
+										className="sr-only"
+									/>
+									<TextInput
+										id="lastname"
+										type="lastname"
+										placeholder="Last Name *"
+										onChange={(e) => setLastName(e.target.value)}
+										required={true}
+										state={lastName}
+										setState={setLastName}
+										className="w-full"
+									/>
+								</div>
+								<div className="flex flex-col gap-2 sm:flex-row sm:gap-3">
+									<GenderDropDown state={gender} setState={setGender} />
+									<DobDatePicker state={birthDate} setState={setBirthDate} />
+								</div>
+								<LocationDropDown state={location} setState={setLocation} />
 								<Label
-									htmlFor="firstname"
-									value="First Name"
+									htmlFor="phone"
+									value="Phone Number"
 									className="sr-only"
 								/>
 								<TextInput
-									id="firstname"
-									type="firstname"
-									placeholder="First Name *"
-									onChange={(e) => setFirstName(e.target.value)}
+									id="phone"
+									type="tel"
+									placeholder="Phone Number (optional)"
+									onChange={(e) => setPhoneNumber(e.target.value)}
+									required={false}
+								/>
+								<Label htmlFor="email" value="Email" className="sr-only" />
+								<TextInput
+									id="email"
+									type="email"
+									placeholder="Email *"
+									onChange={(e) => setEmail(e.target.value)}
 									required={true}
-									className="w-full"
 								/>
 								<Label
-									htmlFor="lastname"
-									value="Last Name"
+									htmlFor="password"
+									value="Password"
 									className="sr-only"
 								/>
-								<TextInput
-									id="lastname"
-									type="lastname"
-									placeholder="Last Name *"
-									onChange={(e) => setLastName(e.target.value)}
-									required={true}
-									state={lastName}
-									setState={setLastName}
-									className="w-full"
+								<PasswordField
+									label="Password *"
+									// onChange={(e) => setPassword(e.target.value)}
+									state={password}
+									setState={setPassword}
+									showPassword={showPassword}
+									setShowPassword={setShowPassword}
 								/>
-							</div>
-							<div className="flex flex-col gap-2 sm:flex-row sm:gap-3">
-								<GenderDropDown state={gender} setState={setGender} />
-								<DobDatePicker state={birthDate} setState={setBirthDate} />
-							</div>
-							<LocationDropDown state={location} setState={setLocation} />
-							<Label htmlFor="phone" value="Phone Number" className="sr-only" />
-							<TextInput
-								id="phone"
-								type="tel"
-								placeholder="Phone Number (optional)"
-								onChange={(e) => setPhoneNumber(e.target.value)}
-								required={false}
-							/>
-							<Label htmlFor="email" value="Email" className="sr-only" />
-							<TextInput
-								id="email"
-								type="email"
-								placeholder="Email *"
-								onChange={(e) => setEmail(e.target.value)}
-								required={true}
-							/>
-							<Label htmlFor="password" value="Password" className="sr-only" />
-							<PasswordField
-								label="Password *"
-								// onChange={(e) => setPassword(e.target.value)}
-								state={password}
-								setState={setPassword}
-								showPassword={showPassword}
-								setShowPassword={setShowPassword}
-							/>
-							{message !== null && (
-								<p
-									id="message-notice"
-									className={`ml-1 text-[.87rem] ${
-										message.status === 'error'
-											? 'text-red-600'
-											: 'text-green-500'
-									} relative top-1`}
-								>
-									<span className="font-medium"></span> {message.message}
-								</p>
-							)}
-							<div>
-								<button
-									className="bg-brand-blue-800 h-10 w-full rounded-3xl text-white font-regular mt-3"
-									type="button"
-									onClick={() => signUp()}
-								>
-									Sign Up
-								</button>
-							</div>
-							<div>
-								<button
-									className="text-brand-blue-800 border border-brand-blue-800 h-10 w-full rounded-3xl bg-white font-regular mb-3"
-									type="button"
-									onClick={() => handleEnterAsGuest()}
-								>
-									Enter as a Guest
-								</button>
-							</div>
-						</form>
+								{message !== null && (
+									<p
+										id="message-notice"
+										className={`ml-1 text-[.87rem] ${
+											message.status === 'error'
+												? 'text-red-600'
+												: 'text-green-500'
+										} relative top-1`}
+									>
+										<span className="font-medium"></span> {message.message}
+									</p>
+								)}
+								<div>
+									<button
+										className="bg-brand-blue-800 h-10 w-full rounded-3xl text-white font-regular mt-3"
+										type="button"
+										onClick={() => signUp()}
+									>
+										Sign Up
+									</button>
+								</div>
+								<div>
+									<button
+										className="text-brand-blue-800 border border-brand-blue-800 h-10 w-full rounded-3xl bg-white font-regular mb-3"
+										type="button"
+										onClick={() => handleEnterAsGuest()}
+									>
+										Enter as a Guest
+									</button>
+								</div>
+							</form>
+						</div>
+						<p className="font-normal text-base cursor-pointer mt-3">
+							Have an account?{' '}
+							<Link href="/login" className="font-bold">
+								Sign In
+							</Link>
+						</p>
 					</div>
-					<p className="font-normal text-base cursor-pointer mt-3">
-						Have an account?{' '}
-						<Link href="/login" className="font-bold">
-							Sign In
-						</Link>
-					</p>
 				</div>
 			</div>
 		</div>
-    </div>
 	);
 }
