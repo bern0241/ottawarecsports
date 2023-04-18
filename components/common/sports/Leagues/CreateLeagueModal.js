34c7174e8a3a0ae149fbce9476988d6ad63336b4
[Justin Bernard] <[bern0241@algonquinlive.com]>
2023-04-18 11:10:16 -0400

    Further increased tabbing within pages

--- components/common/sports/Leagues/CreateLeagueModal.js
+++ components/common/sports/Leagues/CreateLeagueModal.js
@@ -261,8 +261,8 @@ export default function CreateLeagueModal({
 								</>
 							)}
 
-							<div className="flex gap-2 sm:justify-between">
-								<div>
+							<div className="flex gap-3 justify-between">
+								<div className='w-full'>
 									<label
 										htmlFor="numPerPeriod"
 										className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -282,7 +282,7 @@ export default function CreateLeagueModal({
 										className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									/>
 								</div>
-								<div>
+								<div className='w-full'>
 									<label
 										htmlFor="timePerPeriod"
 										className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
